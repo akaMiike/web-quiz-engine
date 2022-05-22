633aa10dab189903b8953b219c33b0c426ca89da
akaMiike <mike.nss@alu.ufc.br>
2022-05-22 01:18:52 -0300

    WIP: Stage 2 - Lots of Quizzes: - Added id and answer fields in Quiz class model - Added new request to create a quiz with optional fields

--- src/main/java/com/hyperskill/webquizengine/model/Quiz.java
+++ src/main/java/com/hyperskill/webquizengine/model/Quiz.java
@@ -1,5 +1,6 @@
 package com.hyperskill.webquizengine.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,14 +8,24 @@ import java.util.List;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(value = {"answer"})
 public class Quiz {
+    private int id;
     private String title;
     private String text;
     private List<String> options;
+    private int answer;
 
-    public Quiz(String title, String text, List<String> options){
+    public Quiz(int id, String title, String text, List<String> options, int answer){
+        this.id = id;
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
     }
+
+    public Quiz(String title, String text, List<String> options){
+        new Quiz(0,title,text,options,-1);
+    }
+
 }
