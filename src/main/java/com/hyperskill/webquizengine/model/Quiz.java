6f5014af816d163369dac7819a9afcbda76ca90a
akaMiike <mike.nss@alu.ufc.br>
2022-07-02 11:10:36 -0300

    Stage 4 - Adding Database - Added Entity Quiz class and H2 Database - Created Service and Repository classes - Updated controller class to get and save quizzes on database

--- src/main/java/com/hyperskill/webquizengine/model/Quiz.java
+++ src/main/java/com/hyperskill/webquizengine/model/Quiz.java
@@ -3,21 +3,38 @@ package com.hyperskill.webquizengine.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name="Quiz")
 public class Quiz {
-    private int id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="title")
     private String title;
+
+    @Column(name="text")
     private String text;
+
+    @ElementCollection
+    @CollectionTable(name="options", joinColumns = @JoinColumn(name="id"))
+    @Column(name="options")
     private List<String> options;
+
+    @ElementCollection
+    @CollectionTable(name="answer", joinColumns = @JoinColumn(name="id"))
+    @Column(name="answer")
     private List<Integer> answer;
 
     public Quiz(){}
 
-    public Quiz(int id, String title, String text, List<String> options, List<Integer> answer){
+    public Quiz(long id, String title, String text, List<String> options, List<Integer> answer){
         this.id = id;
         this.title = title;
         this.text = text;
@@ -26,7 +43,7 @@ public class Quiz {
     }
 
     public Quiz(String title, String text, List<String> options){
-        new Quiz(0,title,text,options,List.of());
+        new Quiz(0,title,text,options, List.of());
     }
 
 }
